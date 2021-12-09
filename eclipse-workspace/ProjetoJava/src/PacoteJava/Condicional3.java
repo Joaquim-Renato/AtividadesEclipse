125faa03ae25acbf8f6ee6c61883c2fa1cd00eae
Joaquim <jocajoaquimas@gmail.com>
2021-12-09 20:55:21 -0300

    Upload inicial

---
+++ eclipse-workspace/ProjetoJava/src/PacoteJava/Condicional3.java
@@ -0,0 +1,28 @@
+package PacoteJava;
+
+import java.util.Scanner;
+
+public class Condicional3 {
+
+	public static void main(String args[]) {
+		
+		Scanner ler = new Scanner(System.in);
+		
+		int idade = 0 ;
+		
+		System.out.println("Digite idade:");
+		idade = ler.nextInt( );
+		
+		
+		if ((idade => 10) && (<= 14 )) {
+			System.out.println("Você pertence a categoria infantil");
+			
+		} else if (idade => 15 || <= 17 ) {
+			System.out.printf("Você pertence a categoria juvenil");{
+				
+				
+				
+			else (idade =18 || >=25 ) {
+				System.out.printf("Você pertence a categoria adulto");{
+	}
+}
