09a836e36a5f5e33f475b8cc9826921b72b0a215
Joaquim <jocajoaquimas@gmail.com>
2021-12-15 00:01:47 -0300

    atualizando

--- ProjetoJava/src/PacoteJava/VetoreseMatrizes/Matriz1.java
+++ ProjetoJava/src/PacoteJava/VetoreseMatrizes/Matriz1.java
@@ -6,49 +6,58 @@ public class Matriz1 {
 	
 	public static void main(String[] args)
 	{
-		Scanner.ler=new Scanner(System.in);
+		Scanner ler=new Scanner(System.in);
 	
 
 	int matrizN1 [][] = new int [4] [6] ;
 	int matrizN2 [][] = new int [4][6];
+	int matrizSoma [][] = new int [4][6];
 	
 	
 	
 		for (int i=0; i<4; i++) {
 		for (int j=0; j<6; j++) {
-			System.out.println("Digite o valor da linha:"+i+"numero");
-			System.out.println("Digite o valor da coluna:"+j+"numero");
-			Object[][] matrizsoma;
-			matrizsoma[i][j]= matrizN1+matrizN2;
-			
-			
-		}
-		}
-		for (int i=0; i<4; i++) {
-			for (int j=0; j<6; j++) {
-				Object[][] matrizmultiplica;
-				matrizmultiplica[i][j]= matrizN1*matrizN2;
-			
-			
-			
-		}
+			System.out.println("\nDigite o valor da linha: "+i+" coluna "+j);
+			matrizN1[i][j]=ler.nextInt();
 		
-	}
-		for (int i=0; i<4; i++) {
-			System.out.println("\n");
-			for (int j=0; j<6; j++) {
-				System.out.print(matrizsoma[i][j]+"\t");
-	}
+			
+			
+			
+			//soma 
+//			matrizSoma[i][j]= matrizN1[i][j]+ matrizN2[i][j];
+					
+			
+		}
 
-}
-		for (int i=0; i<4; i++) {
-			System.out.println("\n");
-			for (int j=0; j<6; j++) {
-				System.out.print(matrizmultiplica[i][j]+"\t");	
+		
+		
+//		}
+//		linha 18 ate linha 30 leu todas as posições da matrizN1
+//		ler todas a posições da matrizN2
+//		somar cada poção especifica da matrizN1 com as posições especificas de N2 e salvar na posição especifica da matrizSoma
+	//	repit so q com multiplicação 
+//	}
+		
+		
+		
+//		for (int i=0; i<4; i++) {
+//			System.out.println("\n");
+//			for (int j=0; j<6; j++) {
+//				System.out.print(matrizsoma[i][j]+"\t");
+//	}
+//
+//}
+//		for (int i=0; i<4; i++) {
+//			System.out.println("\n");
+//			for (int j=0; j<6; j++) {
+//				System.out.print(matrizmultiplica[i][j]+"\t");	
+//	}
+//}
 	}
+		}
 }
-	}
-}
+	
+			
 
 
 
